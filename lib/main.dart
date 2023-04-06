fc83c0a574821fe60637230a48ebb28b3584d778
Abdullah Abo-Sido <odod41907@gmail.com>
2023-04-06 14:55:33 +0200

    Adding Dark Theme to app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'app.dart';
+import 'model_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-
-      debugShowCheckedModeBanner: false,
-      title: 'Speak It',
-      home: SpeakIt(),
+    return ChangeNotifierProvider(
+      create: (_) => ModelTheme(),
+      child: Consumer<ModelTheme>(
+          builder: (context, ModelTheme themeNotifier, child) {
+        return MaterialApp(
+          title: 'Speak It App',
+          theme: themeNotifier.isDark
+              ? ThemeData(brightness: Brightness.dark)
+              : ThemeData(brightness: Brightness.light),
+          themeMode: ThemeMode.system,
+          debugShowCheckedModeBanner: false,
+          home: const SpeakIt(),
+        );
+      }),
     );
   }
 }
